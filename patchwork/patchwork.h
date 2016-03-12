2a561aa5cbf3929b12a408a883cb57f76d207be8
Alex Lee <liyiqiang@baidu.com>
2016-03-12 21:54:46 +0800

    export public headers

--- patchwork/patchwork.h
+++ patchwork/patchwork.h
@@ -18,24 +18,46 @@ FOUNDATION_EXPORT const unsigned char patchworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <patchwork/PublicHeader.h>
 
+// database
 #import <patchwork/ALDatabase.h>
 #import <patchwork/ALDBMigrationProtocol.h>
 #import <patchwork/ALFMDatabaseQueue.h>
 #import <patchwork/ALDBColumnInfo.h>
 #import <patchwork/ALSQLCondition.h>
+
+// extensions
 #import <patchwork/NSArray+ArrayExtensions.h>
 #import <patchwork/BlocksKitExtension.h>
 #import <patchwork/NSString+Heler.h>
+
+// network adaptor
+#import <patchwork/ALHTTPRequest.h>
+#import <patchwork/ALHTTPResponse.h>
+#import <patchwork/ALURLRequestQueueAdaptorProtocol.h>
+#import <patchwork/ASIHTTPRequestQueueAdaptor.h>
+#import <patchwork/NSURLSessionAdaptor.h>
+
+// model
 #import <patchwork/ALModel.h>
+
 #else
+// database
 #import "ALDatabase.h"
 #import "ALDBMigrationProtocol.h"
 #import "ALFMDatabaseQueue.h"
 #import "ALDBColumnInfo.h"
 #import "ALSQLCondition.h"
+// extension
 #import "NSArray+ArrayExtensions.h"
 #import "BlocksKitExtension.h"
 #import "NSString+Heler.h"
+// network adaptor
+#import "ALHTTPRequest.h"
+#import "ALHTTPResponse.h"
+#import "ALURLRequestQueueAdaptorProtocol.h"
+#import "ASIHTTPRequestQueueAdaptor.h"
+#import "NSURLSessionAdaptor.h"
+// model
 #import "ALModel.h"
 #endif
 
